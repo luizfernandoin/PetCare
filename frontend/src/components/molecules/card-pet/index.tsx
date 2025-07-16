eec84d44d3222e19f313b57fbc8d5cc729f3658c
JoseHenrique1 <jh4832184@gmail.com>
2025-07-15 23:25:47 -0300

    test: add test of registration pet

--- frontend/src/components/molecules/card-pet/index.tsx
+++ frontend/src/components/molecules/card-pet/index.tsx
@@ -20,7 +20,7 @@ interface props {
   features: string;
   setPets: React.Dispatch<React.SetStateAction<Pet[]>>
 }
-export default function CardPet({name, breed, size, age, features, image, setPets}: props) {
+export default function CardPet({ name, breed, size, age, features, image, setPets }: props) {
   const [open, setOpen] = useState(false);
 
   const handleDelete = () => {
@@ -34,14 +34,18 @@ export default function CardPet({name, breed, size, age, features, image, setPet
   return (
     <div className="relative max-w-60 rounded overflow-hidden">
       <div className="absolute top-0 w-full flex justify-between p-2 rounded-2xl">
-        <Trash onClick={handleDelete} size={30} color="white" className="bg-red-500 p-1.5 rounded cursor-pointer"/>
-        <Edit onClick={handleEdit} size={30} color="white" className="bg-[#467C98] p-1.5 rounded cursor-pointer" />
+        <span data-testid={`delete-${name}`} >
+          <Trash onClick={handleDelete} size={30} color="white" className="bg-red-500 p-1.5 rounded cursor-pointer" />
+        </span>
+        <span>
+          <Edit onClick={handleEdit} size={30} color="white" className="bg-[#467C98] p-1.5 rounded cursor-pointer" />
+        </span>
       </div>
       <img className="object-cover h-full min-h-96" src={image} alt="Dog" />
       <div className="absolute w-full p-2 text-center bottom-0 bg-[#F0F0F0]">
         {name}
       </div>
-      <ModalEditPet open={open} setOpen={setOpen} name={name} breed={breed} size={size} age={String(age)} features={features} imagem={image} setPets={setPets}/>
+      <ModalEditPet open={open} setOpen={setOpen} name={name} breed={breed} size={size} age={String(age)} features={features} imagem={image} setPets={setPets} />
     </div>
   )
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,9 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   // root: "./src",
-  server: {
-    port: 3000,
-  },
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
