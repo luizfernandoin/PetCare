916cb52ea337d4aaa36e511d4a355f6058cde004
luizfernandoin <luizfernandoinfo100@gmail.com>
2025-07-22 10:14:35 -0300

    refactor: add porta from .env

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use("/api", router);
 app.use(errorMiddleware);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
     
