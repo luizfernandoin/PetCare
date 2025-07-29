a83c6c7700b66746415906589782eed3548dd74a
JoseHenrique1 <jh4832184@gmail.com>
2025-07-29 10:22:04 -0300

    view: add home page

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+import { Link } from 'react-router'
+
+export function Home() {
+  return (
+    <div className='space-y-4'>
+      <h1 className='text-2xl font-bold'>Pagina home</h1>
+      <p>Rota publica para todos os tipos de usuarios</p>
+      <div className='flex gap-4'>
+        <Button><Link to={"/auth/signin"}>Entrar</Link></Button>
+        <Button><Link to={"/dashboard"}>Dashboard</Link></Button>
+        <Button><Link to={"/pets"}>Pets</Link></Button>
+      </div>
+    </div>
+  )
+}
