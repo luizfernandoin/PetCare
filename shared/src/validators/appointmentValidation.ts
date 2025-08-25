ad5d3a65b6661484f339cf392a331300d32f781d
luizfernandoin <luizfernandoinfo100@gmail.com>
2025-08-25 12:05:46 -0300

    feat(appointment): add validation to ensure endTime is after startTime

--- shared/src/validators/appointmentValidation.ts
+++ shared/src/validators/appointmentValidation.ts
@@ -11,6 +11,13 @@ const appointmentSchema = z.object({
     startTime: z.string().regex(/^([01]\d|2[0-3]):([0-5]\d)$/, { message: "Hora de início inválida. Use o formato HH:mm." }),
     endTime: z.string().regex(/^([01]\d|2[0-3]):([0-5]\d)$/, { message: "Hora de início inválida. Use o formato HH:mm." }),
     status: z.enum(["PENDING", "CONFIRMED", "CANCELED", "COMPLETED"], { message: "Status inválido. Os valores permitidos são: pendente, confirmado, cancelado ou completado." })
+}).refine((data) => {
+    const [startHour, startMinute] = data.startTime.split(":").map(Number);
+    const [endHour, endMinute] = data.endTime.split(":").map(Number);
+    const start = startHour * 60 + startMinute;
+    const end = endHour * 60 + endMinute;
+    return end > start;
+}, { message: "Hora de término deve ser depois da hora de início.", path: ["endTime"]
 });
 
 type AppointmentCreate = z.infer<typeof appointmentSchema>
